7a6545b85e2bd6631e5fa0f804009beff42a002a
Arunabha Mukhopadhyay <145916970+Arunabha-Mukhopadhyay@users.noreply.github.com>
2024-06-22 14:25:07 +0000

    conversion of datatypes

---
+++ 01.basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let age = "18"
+console.log(typeof age)
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+
+let name = ""
+console.log(Boolean(name))
+
+//"33abc" = NaN
+//null = 0
+//true = 1
+// undefined = NaN
+//empty string = false
+
