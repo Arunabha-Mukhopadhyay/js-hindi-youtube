68d61cdf0186ea613a26df4b8a742a92ee0a9f40
Arunabha Mukhopadhyay <145916970+Arunabha-Mukhopadhyay@users.noreply.github.com>
2024-06-22 05:12:14 +0000

    datatypes

---
+++ 01.basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all the code as new version of javascript
+
+let age = 18;
+let name = "arunabha mukhopadhyay"
+let isLogged = false
+
+//datatypes
+//1. basic datatypes:
+/*
+ number 
+ bigint
+ strings
+ boolean
+ null => standalone value, typeof is object
+ undefined   type of is undefined;
+*/
+
+//object
