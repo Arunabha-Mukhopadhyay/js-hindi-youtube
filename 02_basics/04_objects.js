500dd8d4f5155fb2d4280471a8187a3dbeda74e5
Arunabha Mukhopadhyay <145916970+Arunabha-Mukhopadhyay@users.noreply.github.com>
2024-06-25 11:23:34 +0000

    done jason

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,7 +26,7 @@ const obj2 = {3:'c', 4:'d'}
 // const obj4 = Object.assign({},obj1,obj2)
 
 const obj3 = {...obj1, ...obj2}     // spread operator
-console.log(obj3)
+// console.log(obj3)
 // console.log(obj4)
 // console.log(obj4 === obj1)
 
@@ -43,18 +43,27 @@ const user = [
     }
 ]
 
-console.log(user[0].email)
-console.log(Object.keys(tinderUser))    // output datatype : array
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(user[0].email)
+// console.log(Object.keys(tinderUser))    // output datatype : array
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.hasOwnProperty('isLogged'))
 
 
 const courses = {
+    
     coursesName: "ja in hindi",
     prise: "999",
     courseInstrustor: "hitesh"
 }
-const {courseInstrustor} = courses
-console.log(courseInstrustor)
+const {courseInstrustor : instructor} = courses
+console.log(instructor)
+
+// JASON:
+
+// {
+//     "name": "arunabha",
+//     "email": "arunbhha@gmail.com",
+//     "id": "123"
+// }
